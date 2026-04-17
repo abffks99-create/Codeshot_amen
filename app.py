d9815503e77d366747b5b62fc829df6189b54684
seonkyo9582 <seonkyo9582@gmail.com>
2026-04-17 12:14:05 +0900

    수정 사항 업로드

--- app.py
+++ app.py
@@ -857,4 +857,4 @@ def withdraw():
     session.clear(); return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
